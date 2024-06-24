35fc41546490a249937fb14bc8d83e4b51ef1861
saadsgh123 <sdsghouri@gmail.com>
2024-06-24 22:04:42 +0100

    MySQL setup development (flush)

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 --creates a database a new user with specified permissions
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+FLUSH PRIVILIGES;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';

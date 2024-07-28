5842eed598ca7a44b1a17d3796eb17acb3fb5aed
saadsgh123 <sdsghouri@gmail.com>
2024-07-28 23:06:06 +0100

    kmklnlkn ✅

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,14 +5,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    return 'Hello HBNB!'
-
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
-    return 'HBNB'
+    """ def doc """
+    return "Hello HBNB!"
 
 
-if __name__ == '__main__':
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def doc """
+    return "HBNB"
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
